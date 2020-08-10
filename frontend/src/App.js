3945f4d8d23d37c3785af8cbb61ffa028bebac3d
deepak <dk721572@gmail.com>
2020-08-10 13:25:49 +0530

    🗑 removed unnecessary files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Room from './routes/Room';
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
     api: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://shrouded-plateau-06981.herokuapp.com'
 }
